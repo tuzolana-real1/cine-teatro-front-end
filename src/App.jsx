a9633d84aacaa5b9bb78dcf77ddcb674cedbd3bc
tuzolana-real1 <fidelreal38@gmail.com>
2026-03-17 22:54:22 +0100

    Setup inicial com Vite, Tailwind e axios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+     <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-6xl">
+        CineTeatro
+      </h1>
+    </div>
+  )
+}
+
+export default App
